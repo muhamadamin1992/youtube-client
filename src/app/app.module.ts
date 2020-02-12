98af4981249d875334e6c7225ab5b5fe59299287
Muhamadamin <muhamadamin1992@gmail.com>
2020-02-12 15:21:58 +0500

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { SortingComponent } from './components/sorting/sorting.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SortingComponent,
+    SearchResultsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
