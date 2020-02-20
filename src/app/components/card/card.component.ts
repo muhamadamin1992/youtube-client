89d0e7d22e05aaf734abe19a5e9248251d6d9c44
Muhamadamin <muhamadamin1992@gmail.com>
2020-02-20 18:27:33 +0500

    feat: implement the colorful border under the search result item

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -4,15 +4,25 @@ import { ICard } from '../../models/card.model';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
 
+  @Input() public card: ICard;
+  public borderClass: string;
+
   constructor() { }
 
-  @Input() card: ICard
-
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    const now: Number = Date.now();
+    const offset: Number = +now - +new Date(this.card.snippet.publishedAt);
+    if (offset < 1000 * 60 * 60 * 24 * 7) {
+      this.borderClass = 'card--bd-blue';
+    } else if (offset < 1000 * 60 * 60 * 24 * 31) {
+      this.borderClass = 'card--bd-green';
+    } else {
+      this.borderClass = 'card--bd-red';
+    }
   }
 
 }
